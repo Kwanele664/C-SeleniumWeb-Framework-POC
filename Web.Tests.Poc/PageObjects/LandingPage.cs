b0ce1119242df5388180576fe0af4aebac385e72
Kwanele Nzimande <kwanele.nzimande@gmail.com>
2022-10-10 13:55:17 +0200

    Adding PageObjects classes

---
+++ Web.Tests.Poc/PageObjects/LandingPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace Web.Tests.Poc.PageObjects;
+
+public class LandingPage : BasePage
+{
+    private readonly IWebDriver _driver;
+
+    public LandingPage(IWebDriver driver) : base(driver)
+    {
+        _driver = driver;
+    }
+    
+    private static By ProductLabel => By.ClassName("title");
+
+    public string GetProductText()
+    {
+        return  GetText(ProductLabel);
+    }
+}
---
+++ Web.Tests.Poc/Reporting/ExtentService.cs
@@ -0,0 +1,24 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using ExtentReports = AventStack.ExtentReports.ExtentReports;
+
+namespace Web.Tests.Poc.Reporting;
+
+public static class ExtentService
+{
+    private static readonly Lazy<AventStack.ExtentReports.ExtentReports> Lazy =
+        new Lazy<AventStack.ExtentReports.ExtentReports>(() => new AventStack.ExtentReports.ExtentReports());
+    public static AventStack.ExtentReports.ExtentReports Instance => Lazy.Value;
+
+    static ExtentService()
+    {
+        var reporter = new ExtentHtmlReporter("get current directory" + "/Reports/")
+        {
+            Config =
+            {
+                Theme = Theme.Standard
+            }
+        };
+        Instance.AttachReporter(reporter);
+    }
+}
