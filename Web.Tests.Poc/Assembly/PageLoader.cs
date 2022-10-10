67f233e3512f3990a6b9fbbdf3d3e31fd2af0e35
Kwanele Nzimande <kwanele.nzimande@gmail.com>
2022-10-10 14:18:15 +0200

    Adding Assembly for Page loader and ExtentTestMananger

---
+++ Web.Tests.Poc/Assembly/PageLoader.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using Web.Tests.Poc.PageObjects;
+
+namespace Web.Tests.Poc.Assembly;
+
+public class PageLoader
+{
+    private readonly IWebDriver _driver;
+
+    public PageLoader(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    public LoginPage LoginPage => GetPage<LoginPage>(_driver);
+    public LandingPage LandingPage => GetPage<LandingPage>(_driver);
+    
+    private static T GetPage<T>(IWebDriver driver) where T : class
+    {
+        return (T)Activator.CreateInstance(typeof(T), driver)! 
+               ?? throw new InvalidOperationException();
+    }
+}
---
+++ Web.Tests.Poc/Reporting/ExtentTestManager.cs
@@ -0,0 +1,22 @@
+﻿using AventStack.ExtentReports;
+using System.Threading;
+
+namespace Web.Tests.Poc.Reporting;
+
+
+public class ExtentTestManager
+{
+    private static readonly ThreadLocal<ExtentTest> ParentTest = new ThreadLocal<ExtentTest>();
+
+    private static readonly object Synclock = new object();
+
+    // creates a parent test
+    public static ExtentTest CreateTest(string testName, string description = null)
+    {
+        lock (Synclock)
+        {
+            ParentTest.Value = ExtentService.Instance.CreateTest(testName, description);
+            return ParentTest.Value;
+        }
+    }
+}
